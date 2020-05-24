99d395020b98b9be2d91f934d480b44b5b11d8b2
RH1532 <65445926+RH1532@users.noreply.github.com>
2020-05-24 15:12:45 +0300

    Add files via upload

---
+++ laba4.2.cpp
@@ -0,0 +1,33 @@
+﻿#include"Class_Haf.h"
+#include<iomanip>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	code test;
+	string str = "";
+	string save = "";
+	int save1[255];
+	cout << "enter a string" << endl;
+	getline(cin, str);
+	const int N = 255;
+	int counting[N];
+	for (int i = 0; i < N; i++) {
+		counting[i] = 0;
+	}
+	test.count(str, counting);
+	test.print(str, counting, save, save1);
+	cout << "this string takes" << " " << str.length() * 8 << " " << "bit" << endl;
+	string testing = test.sort(save, save1);
+	test.tree(testing, save1);
+	node<trenode<char>>* save5 = nod.Head;
+
+	test.coding(nod.Head, save1);
+	test.code_main(nod.Head);
+	test.print2(tre2.Head, testing);
+	string help = test.finals(tre2.Head, str);
+	cout << "final string" << " " << "-" << " " << help << endl;
+	cout << "this string takes" << " " << help.length() << " " << "bit" << endl;
+}
